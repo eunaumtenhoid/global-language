131846cb7d3563ca31104dbb981def232248cd76
eunaumtenhoid <eunaumtenhoid@outlook.com>
2018-02-13 09:50:23 -0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# global-language
+# global language
+
+## Installation
+
+Copy the extension to phpBB/ext/eunaumtenhoid/globallanguage
+
+Go to "ACP" > "Customise" > "Extensions" and enable the "global language" extension.
+
+## License
+
+[GPLv2](license.txt)
---
+++ language/en/common.php
@@ -0,0 +1,30 @@
+<?php
+/**
+*
+* global language extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2018 eunaumtenhoid <https://github.com/phpBBTraducoes>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+// Create the lang array if it does not already exist
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// Merge the following language entries into the lang array
+$lang = array_merge($lang, array(
+	'TYPE_UPLOAD' 			=> 'Version',
+	'TOPIC_ICON' 			=> 'teste',
+	
+));
